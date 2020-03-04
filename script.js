b38fbb679d0e2a2352ba666b52672b182d4817a8
Helen Lin <30956318+hlin98@users.noreply.github.com>
2020-03-04 14:12:08 -0500

    styling and screen size issue

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-var count = 1
-    function likeButton(btn) {
-        var property = document.getElementById("like-button");
-        if (count == 0) {
-            property.style.color= "gray"
-            count = 1;        
-        }
-        else {
-            property.style.color= "red"
-            count = 0;
-        }
-    }
-
 function change(){
     var card = document.querySelector('.card');
     var saveButton = document.getElementById("saveBtn");
