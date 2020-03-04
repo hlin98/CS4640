3bb72895cf3734402ccb075f34c776965c98d69a
Helen Lin <30956318+hlin98@users.noreply.github.com>
2020-03-04 13:26:58 -0500

    add new styles

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ var count = 1
             count = 0;
         }
     }
+
+function change(){
+    var card = document.querySelector('.card');
+    var saveButton = document.getElementById("saveBtn");
+    console.log(card.style);
+    if (saveBtn.value =="Save"){
+        saveBtn.value = "Saved!";
+        saveBtn.style.backgroundColor = "#c71a2e";
+        card.style.boxShadow = "10px 10px 25px -3px rgba(255,66,88,0.1)";
+
+    }
+    else{
+        saveBtn.value = "Save";
+        saveBtn.style.backgroundColor = "#FF4258";
+        card.style.boxShadow = "10px 10px 25px -3px rgba(0,0,0,0.1)";
+    }
+    }
